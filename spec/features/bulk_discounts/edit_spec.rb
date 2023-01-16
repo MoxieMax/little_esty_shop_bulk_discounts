4420cd2d174c1a1d2ff7b2193104fd932cf8ac8b
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-16 01:56:22 -0700

    Completed user story 5

--- spec/features/bulk_discounts/edit_spec.rb
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'bulk discount show' do
   before :each do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     
-    @discount1 = BulkDiscount.create!(discount_percentage: 5, quantity_threshold: 10, merchant_id: @merchant1.id)
+    @discount1 = BulkDiscount.create!(discount_percentage: 11, quantity_threshold: 10, merchant_id: @merchant1.id)
     
-    edit_merchant_bulk_discount_path(@merchant1, @discount1)
+    visit edit_merchant_bulk_discount_path(@merchant1, @discount1)
   end
   
   describe 'user story 5 #edit' do
@@ -14,16 +14,17 @@ RSpec.describe 'bulk discount show' do
     # # When I visit my bulk discount show page
     # # Then I see a link to edit the bulk discount
     # # When I click this link
-    it 'the edit page populates with filled forms' do
-      # # Then I am taken to a new page with a form to edit the discount
+    it 'the edit page populates with filled fields' do
       save_and_open_page
-      expect(page).to have_field("Discount percentage")
+      # # Then I am taken to a new page with a form to edit the discount
       # # And I see that the discounts current attributes are pre-poluated in the form
-      expect(page).to have_content(@discount1.discount_percentage)
+      expect(page).to have_field("Discount percentage", with: 11)
       
-      expect(page).to have_field("Quantity threshold")
-      expect(page).to have_content(@discount1.quantity_threshold)
       
+      expect(page).to have_field("Quantity threshold", with: 10)
+    end
+      
+    it 'can have the form filled in and submitted' do
       # # When I change any/all of the information and click submit
       fill_in 'Discount percentage', with: 15
       fill_in 'Quantity threshold', with: 20
@@ -32,11 +33,11 @@ RSpec.describe 'bulk discount show' do
       # # Then I am redirected to the bulk discount's show page
       expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant1, @discount1))
       
+      click_on "Submit"
+      
       # # And I see that the discount's attributes have been updated
-      within ("#discount-#{@discount1.id}") do
-        expect(page).to have_content(15)
-        expect(page).to have_content(20)
-      end
+      expect(page).to have_content(15)
+      expect(page).to have_content(20)
     end
   end
 end
