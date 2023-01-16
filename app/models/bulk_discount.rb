b834f46974a45ab9763dbeb97cfe96b8e55c9513
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-15 18:18:04 -0700

    Updated some spacing, double checked validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,9 +1,9 @@
 class BulkDiscount < ApplicationRecord
+  validates :discount_percentage, numericality: { only_integer: true }, presence: true
+  validates :quantity_threshold, numericality: { only_integer: true }, presence: true
+  
   belongs_to :merchant
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
-
-  validates :discount_percentage, numericality: { only_integer: true }, presence: true
-  validates :quantity_threshold, numericality: { only_integer: true }, presence: true
 end
